40ac91010e31c6d598963f70d156e79b8c61f8da
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-05-19 03:19:25 -0300

    feat: configura o servidor actix-web e o bootstrap na função `main`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web::Data, HttpServer};
+use config::{
+    app::{AppConfig, RustEnv},
+    inertia::get_inertia,
+    vite::get_vite,
+};
+use env_logger::Target;
+use libs::actix::server::get_server;
+
+mod infra;
+mod libs;
+
+#[actix_web::main]
+async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv().expect("Não foi possível inicializar as variáveis de ambiente.");
+
+    env_logger::builder()
+        .parse_env("RUST_LOG")
+        .target(Target::Stdout)
+        .init();
+
+    let app_config = AppConfig::get();
+
+    let is_production_env = app_config.environment == RustEnv::Production;
+    let mut ssr_server_process = None;
+
+    let vite = get_vite().await?;
+    let inertia = Data::new(get_inertia(vite).await?);
+    let inertia_data = inertia.clone();
+
+    let port = if is_production_env { 80 } else { 3000 };
+    let host = if is_production_env {
+        "0.0.0.0"
+    } else {
+        "127.0.0.1"
+    };
+
+    let http_server =
+        HttpServer::new(move || get_server().app_data(inertia_data.clone())).bind((host, port))?;
+
+    if is_production_env {
+        ssr_server_process = inertia
+            .start_node_server("dist/ssr/ssr.js".to_string())?
+            .into();
+    }
+
+    log::info!("Iniciando o servidor em {host}:{port}.");
+    let signal = http_server.run().await;
+
+    if let Some(process) = ssr_server_process {
+        let _ = process.kill().await;
+    }
+
+    signal?;
+
+    Ok(())
 }
