876cad8cd0fc376200670ebd84de2de671dc7f77
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-05-19 03:57:54 -0300

    fix(vite e ts config): arruma as pathaliases no vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,28 @@ import react from "@vitejs/plugin-react";
 import laravel from "laravel-vite-plugin";
 import type { UserConfig } from "vite";
 
+import tsconfig from "./tsconfig.json";
+import path from "node:path";
+
+export const tsconfigPathAliases = Object.fromEntries(
+  Object.entries(tsconfig.compilerOptions.paths).map(([key, values]) => {
+    let value = values[0];
+    if (key.endsWith("/*")) {
+      key = key.slice(0, -2);
+      value = value.slice(0, -2);
+    }
+
+    const nodeModulesPrefix = "node_modules/";
+    if (value.startsWith(nodeModulesPrefix)) {
+      value = value.replace(nodeModulesPrefix, "");
+    } else {
+      value = path.join(__dirname, value);
+    }
+
+    return [key, value];
+  }),
+);
+
 export default {
   plugins: [
     tailwindcss(),
@@ -15,6 +37,9 @@ export default {
     }),
   ],
   publicDir: "/public",
+  resolve: {
+    alias: tsconfigPathAliases
+  },
   server: {
     watch: {
       ignored: ["*"],
