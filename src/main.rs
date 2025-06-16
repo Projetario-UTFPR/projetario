5f3fa24c02cf709f64ef2194f0626bfdbe9fc16d
KaioFelps <kaiofelipe2005@gmail.com>
2025-06-15 22:44:44 -0300

    build: adiciona a feature `dockerimgb` que faz o servidor escutar na porta padrão independentemente da variável `RUST_ENV`
    Essa feature será usada no `Dockerfile`, pois os containers tem suas portas vinculadas à uma porta do host na hora de iniciar a imagem em um container.

--- src/main.rs
+++ src/main.rs
@@ -37,8 +37,17 @@ async fn main() -> anyhow::Result<()> {
     let inertia = Data::new(get_inertia(vite).await?);
     let inertia_data = inertia.clone();
 
-    let port = if is_production_env { 80 } else { 3000 };
-    let host = if is_production_env {
+    // Se estiver sendo compilado em uma imagem Docker, é necessário que escute na porta padrão
+    // no IP padrão, de modo que seja possível levantá-lo como um container docker e fazer o bind
+    // dessa porta em outra porta (como está sendo feito no arquivo `compose.yaml`.)
+    #[cfg(feature = "dockerimgb")]
+    let must_listen_to_public_port = true;
+    #[cfg(not(feature = "dockerimgb"))]
+    let must_listen_to_public_port = is_production_env;
+
+    let port = if must_listen_to_public_port { 80 } else { 3000 };
+
+    let host = if must_listen_to_public_port {
         "0.0.0.0"
     } else {
         "127.0.0.1"
