b9ffbbeaee8517e70a5af5b0c22c8779257b1477
KaioFelps <kaiofelipe2005@gmail.com>
2025-06-21 14:59:54 -0300

    test: adiciona um hasher e comparador fake de senhas para testes

---
+++ src/utils/test/comparador_e_hasher_de_senhas.rs
@@ -0,0 +1,29 @@
+use crate::dominio::autenticacao::{
+    ComparadorDeHashDeSenha,
+    ComparadorEHasherDeSenha,
+    HasherDeSenha,
+};
+
+pub struct ComparadorEHasherDeSenhaFake {
+    sufix: &'static str,
+}
+
+impl ComparadorEHasherDeSenhaFake {
+    pub fn novo() -> Self {
+        Self {
+            sufix: "--fake-hash",
+        }
+    }
+}
+
+impl ComparadorDeHashDeSenha for ComparadorEHasherDeSenhaFake {
+    fn compare(&self, senha_crua: &str, hash: &str) -> bool {
+        hash.strip_suffix(self.sufix)
+            .map(|original| original.eq(senha_crua))
+            .unwrap_or(false)
+    }
+}
+
+impl HasherDeSenha for ComparadorEHasherDeSenhaFake {
+    fn aplique_hash(&self, senha_crua: &str) -> String { format!("{senha_crua}{}", self.sufix) }
+}
