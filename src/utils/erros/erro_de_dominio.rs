a99bd91893053bac3b67e8ca7836a06b9e14df5a
KaioFelps <kaiofelipe2005@gmail.com>
2025-06-21 02:19:19 -0300

    chore(ErroDeDominio): separa o enum `TipoErroDeDominio` em um arquivo separado e adiciona o apelido de tipo `ResultadoDominio` para `Result<T, ErroDeDominio>`

--- src/utils/erros/erro_de_dominio.rs
+++ src/utils/erros/erro_de_dominio.rs
@@ -2,25 +2,7 @@ use std::fmt::Display;
 
 use thiserror::Error;
 
-#[derive(Debug, Clone)]
-pub enum TipoErroDeDominio {
-    Interno,
-    Integridade,
-    ValorInvalido,
-    NãoAutorizado,
-}
-
-impl TipoErroDeDominio {
-    pub fn como_codigo(&self) -> String {
-        match self {
-            Self::Integridade => "integridade",
-            Self::Interno => "interno",
-            Self::NãoAutorizado => "não_autorizado",
-            Self::ValorInvalido => "valor_inválido",
-        }
-        .into()
-    }
-}
+use crate::utils::erros::tipo_erro_de_dominio::TipoErroDeDominio;
 
 #[derive(Debug, Error)]
 pub struct ErroDeDominio {
--- src/utils/erros/mod.rs
+++ src/utils/erros/mod.rs
@@ -1,1 +1,3 @@
 pub mod erro_de_dominio;
+pub mod resultado_de_dominio;
+pub mod tipo_erro_de_dominio;
