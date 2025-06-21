bc4ae61c846327baa1e53b6c879066e920457523
KaioFelps <kaiofelipe2005@gmail.com>
2025-06-21 14:57:29 -0300

    chore(RepositorioDeUsuarios): substitui resultado com ErroDeDominio como erro por  `ResultadoDominio`

--- src/dominio/identidade/repositorios/usuarios.rs
+++ src/dominio/identidade/repositorios/usuarios.rs
@@ -2,14 +2,14 @@ use async_trait::async_trait;
 
 use crate::dominio::identidade::entidades::aluno::Aluno;
 use crate::dominio::identidade::entidades::professor::Professor;
-use crate::utils::erros::erro_de_dominio::ErroDeDominio;
+use crate::utils::erros::ResultadoDominio;
 
 #[async_trait]
 pub trait RepositorioDeUsuarios {
     async fn encontre_professor_pelo_email(
         &self,
         email: &str,
-    ) -> Result<Option<Professor>, ErroDeDominio>;
+    ) -> ResultadoDominio<Option<Professor>>;
 
-    async fn encontre_aluno_pelo_ra(&self, ra: &str) -> Result<Option<Aluno>, ErroDeDominio>;
+    async fn encontre_aluno_pelo_ra(&self, ra: &str) -> ResultadoDominio<Option<Aluno>>;
 }
