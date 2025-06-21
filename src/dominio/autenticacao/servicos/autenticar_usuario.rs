82ccea9e3122be928d53017711b5a0b1612df5fd
KaioFelps <kaiofelipe2005@gmail.com>
2025-06-21 15:56:35 -0300

    test(ServicoAutenticarUsuario): adiciona testes unitários

--- src/dominio/autenticacao/servicos/autenticar_usuario.rs
+++ src/dominio/autenticacao/servicos/autenticar_usuario.rs
@@ -18,6 +18,7 @@ pub struct AutenticarUsuarioParams<'this> {
     pub senha: &'this str,
 }
 
+#[derive(Debug)]
 pub enum AutenticarUsuarioResult {
     Autenticado(UsuarioModelo),
     NaoAutenticado,
@@ -37,6 +38,13 @@ where
     RU: RepositorioDeUsuarios,
     Comparador: ComparadorDeHashDeSenha,
 {
+    pub fn novo(comparador: Comparador, repositorio_usuarios: RU) -> Self {
+        Self {
+            comparador,
+            repositorio_usuarios,
+        }
+    }
+
     pub async fn executar(
         &self,
         params: AutenticarUsuarioParams<'_>,
@@ -80,4 +88,142 @@ where
     }
 }
 
-// TODO: adicionar testes unitários para verificar se o service está certo
+#[cfg(test)]
+mod test {
+    use std::result;
+
+    use rstest::rstest;
+
+    use crate::dominio::autenticacao::HasherDeSenha;
+    use crate::dominio::autenticacao::servicos::autenticar_usuario::{
+        AutenticarUsuarioParams,
+        AutenticarUsuarioResult,
+        ServicoAutenticarUsuario,
+        TipoDeLogin,
+    };
+    use crate::utils::test::comparador_e_hasher_de_senhas::ComparadorEHasherDeSenhaFake;
+    use crate::utils::test::fabricas_de_entidades::usuario_modelo::{
+        FabricaUsuarioModelo,
+        UsuarioModeloConstrutor,
+    };
+    use crate::utils::test::repositorios_em_memoria::fabricas::fabrica_repositorio_de_usuarios::FabricaRepositorioDeUsuarios;
+    use crate::utils::test::repositorios_em_memoria::usuarios::RepositorioDeUsuariosEmMemoria;
+
+    type Sut =
+        ServicoAutenticarUsuario<ComparadorEHasherDeSenhaFake, RepositorioDeUsuariosEmMemoria>;
+
+    #[rstest::fixture]
+    fn sut() -> Sut {
+        let repositorio_usuarios = FabricaRepositorioDeUsuarios::obtenha_repositorio();
+        let comparador = ComparadorEHasherDeSenhaFake::novo();
+
+        let mut professor = UsuarioModeloConstrutor {
+            email: Some("professor@utfpr.edu.br".into()),
+            senha_hash: Some(comparador.aplique_hash("123456")),
+            ..Default::default()
+        };
+
+        repositorio_usuarios
+            .usuarios_tbl
+            .lock()
+            .unwrap()
+            .push(FabricaUsuarioModelo::obtenha_entidade(professor));
+
+        let mut aluno = UsuarioModeloConstrutor::aluno();
+        aluno.registro_aluno = Some("a2600554".into());
+        aluno.senha_hash = Some(comparador.aplique_hash("123456"));
+
+        repositorio_usuarios
+            .usuarios_tbl
+            .lock()
+            .unwrap()
+            .push(FabricaUsuarioModelo::obtenha_entidade(aluno));
+
+        ServicoAutenticarUsuario::novo(comparador, repositorio_usuarios)
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn deveria_rejeitar_registro_de_aluno_que_nao_inicia_com_a_letra_a(sut: Sut) {
+        let resultado = sut
+            .executar(AutenticarUsuarioParams {
+                login: TipoDeLogin::RegistroDeAluno("2600445"),
+                senha: "foo",
+            })
+            .await;
+
+        assert!(resultado.is_err());
+        assert!(resultado.unwrap_err().mensagem().contains("letra \"a\""));
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn deveria_autenticar_um_usuario_valido(sut: Sut) {
+        let resultado = sut
+            .executar(AutenticarUsuarioParams {
+                login: TipoDeLogin::RegistroDeAluno("a2600554"),
+                senha: "123456",
+            })
+            .await;
+
+        assert!(resultado.is_ok());
+
+        let resultado = resultado.unwrap();
+        assert!(matches!(resultado, AutenticarUsuarioResult::Autenticado(_)));
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn deveria_autenticar_um_professor(sut: Sut) {
+        let resultado = sut
+            .executar(AutenticarUsuarioParams {
+                login: TipoDeLogin::EmailInstitucional("professor@utfpr.edu.br"),
+                senha: "123456",
+            })
+            .await;
+
+        assert!(resultado.is_ok());
+
+        assert!(matches!(
+            resultado.unwrap(),
+            AutenticarUsuarioResult::Autenticado(_)
+        ));
+    }
+
+    #[rstest]
+    #[case(TipoDeLogin::EmailInstitucional("foo@gmail.com"))]
+    #[case(TipoDeLogin::RegistroDeAluno("a0000001"))]
+    #[tokio::test]
+    async fn deveria_informar_nao_autenticacao_quando_o_usuario_nao_eh_encontrado(
+        #[case] login: TipoDeLogin<'_>,
+        sut: Sut,
+    ) {
+        let resultado = sut
+            .executar(AutenticarUsuarioParams {
+                login,
+                senha: "123",
+            })
+            .await;
+
+        assert!(resultado.is_ok());
+        assert!(matches!(
+            resultado.unwrap(),
+            AutenticarUsuarioResult::NaoAutenticado
+        ));
+    }
+
+    async fn deveria_informar_nao_autenticacao_quando_a_senha_esta_errada(sut: Sut) {
+        let resultado = sut
+            .executar(AutenticarUsuarioParams {
+                login: TipoDeLogin::EmailInstitucional("professor@utfpr.edu.br"),
+                senha: "123",
+            })
+            .await;
+
+        assert!(resultado.is_ok());
+        assert!(matches!(
+            resultado.unwrap(),
+            AutenticarUsuarioResult::NaoAutenticado
+        ));
+    }
+}
