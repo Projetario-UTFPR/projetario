806857b9cfe93b7795f8cf9a1b9cce1e65939549
KaioFelps <kaiofelipe2005@gmail.com>
2025-07-03 00:35:54 -0300

    feat(actix server): compartilha a propriedade `autenticacao` com o usuario autenticado globalmente nas páginas Inertia

--- src/libs/actix/server.rs
+++ src/libs/actix/server.rs
@@ -9,12 +9,13 @@ use actix_web::{App, HttpMessage, web};
 use config::app::{AppConfig, RustEnv};
 use futures_util::FutureExt;
 use inertia_rust::actix::InertiaMiddleware;
-use inertia_rust::{InertiaProp, hashmap};
+use inertia_rust::{InertiaProp, InertiaService, IntoInertiaPropResult, hashmap};
 use inertia_sessions::file_session::FileSessionStore;
 use inertia_sessions::middlewares::garbage_collector::GarbageCollectorMiddleware;
 use inertia_sessions::middlewares::reflash_temporary_session::ReflashTemporarySessionMiddleware;
 use serde_json::Map;
 
+use crate::dominio::identidade::traits::IntoUsuarioModelo;
 use crate::infra::http::controllers::Controller;
 use crate::infra::http::controllers::autenticacao::ControllerAutenticacao;
 use crate::infra::http::controllers::professores::projetos_de_extensao::ControllerProjetosDeExtensao;
@@ -39,13 +40,31 @@ pub fn get_server() -> App<
     App::new()
         .wrap(GarbageCollectorMiddleware)
         .wrap(InertiaMiddleware::new().with_shared_props(Arc::new(|req| {
+            let usuario = req.extensions().get::<UsuarioDaRequisicao>().unwrap_or_else(|| {
+                log::warn!("Usuário da requisição não encontrada pelo `InertiaMiddleware`, caindo para o usuário convidado.");
+                &UsuarioDaRequisicao::Convidado
+            }).clone();
+            
+            let usuario = match usuario {
+                UsuarioDaRequisicao::Convidado => None,
+                UsuarioDaRequisicao::Aluno(aluno) => Some(aluno.into_usuario_modelo()),
+                UsuarioDaRequisicao::Professor(professor) => Some(professor.into_usuario_modelo())
+            };
+
+            let autenticacao = usuario.map(|usuario| {
+                hashmap!["usuario".to_string() => usuario]
+            });
+
             let flash = req
                 .get_session()
                 .remove(app_config.sessions_flash_key)
                 .map(|flash_map| serde_json::from_str::<Map<_, _>>(&flash_map).unwrap_or_default())
                 .unwrap_or_default();
 
-            async { hashmap![ "flash" => InertiaProp::always(flash) ] }.boxed_local()
+            async { hashmap![
+                "flash" => InertiaProp::always(flash),
+                "autenticacao" => InertiaProp::Data(autenticacao.into_inertia_value()) 
+            ] }.boxed_local()
         })))
         .wrap(ReflashTemporarySessionMiddleware)
         .wrap(MiddlewareUsuarioDaRequisicao)
