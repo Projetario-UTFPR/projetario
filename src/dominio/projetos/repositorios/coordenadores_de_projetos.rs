e7e459596563826770a629b7a7ec857cd80e74b3
João Ricardo <jrzr432@gmail.com>
2025-07-07 12:21:46 -0300

    correcao3

--- src/dominio/projetos/repositorios/coordenadores_de_projetos.rs
+++ src/dominio/projetos/repositorios/coordenadores_de_projetos.rs
@@ -4,7 +4,7 @@ use std::vec;
 
 use async_trait::async_trait;
 use futures_util::FutureExt;
-use sqlx::{AnyPool, Connection, Executor, FromRow, PgPool, Pool, Postgres};
+use sqlx::{AnyPool, Connection, Executor, FromRow, PgPool, Pool, Postgres, QueryBuilder};
 use uuid::Uuid;
 
 use crate::dominio::identidade::entidades::professor::Professor;
@@ -12,23 +12,6 @@ use crate::dominio::projetos::entidades::projeto::Projeto;
 use crate::dominio::projetos::enums::tipo_de_coordenacao::TipoDeCoordenacao;
 use crate::dominio::projetos::enums::tipo_de_projeto::TipoDeProjeto;
 use crate::utils::erros::erro_de_dominio::ErroDeDominio;
-
-#[async_trait]
-pub trait RepositorioDeCoordenadoresDeProjetos {
-    /// Persiste o `projeto` e, imediatamente, associa-o com o professor responsável por ele.
-    async fn criar_projeto_com_coordenador(
-        &self,
-        projeto: &Projeto,
-        coordenador: &Professor,
-    ) -> Result<(), ErroDeDominio>;
-
-    async fn buscar_projetos(
-        &self,
-        filtro: Filtro,
-        ordenador: Ordenador,
-        paginacao: Paginacao,
-    ) -> Result<ProjetosPaginados, ErroDeDominio>;
-}
 
 pub enum Ordenador {
     Data(Ordering),
@@ -45,4 +28,20 @@ pub struct Paginacao {
 pub struct ProjetosPaginados {
     pub projetos: Vec<Projeto>,
     pub qtd_por_pagina: u8,
+}
+#[async_trait]
+pub trait RepositorioDeCoordenadoresDeProjetos {
+    /// Persiste o `projeto` e, imediatamente, associa-o com o professor responsável por ele.
+    async fn criar_projeto_com_coordenador(
+        &self,
+        projeto: &Projeto,
+        coordenador: &Professor,
+    ) -> Result<(), ErroDeDominio>;
+
+    async fn buscar_projetos(
+        &self,
+        filtro: Filtro,
+        ordenador: Ordenador,
+        paginacao: Paginacao,
+    ) -> Result<ProjetosPaginados, ErroDeDominio>;
 }
