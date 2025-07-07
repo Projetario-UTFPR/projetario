4ea1f1f7323152eaa10bd10a7739b8cec8d265b0
KaioFelps <kaiofelipe2005@gmail.com>
2025-07-06 23:39:17 -0300

    chore(novo-projeto-de-extensao): adiciona um título na página

--- www/pages/professores/projetos/novo-projeto-de-extensao.tsx
+++ www/pages/professores/projetos/novo-projeto-de-extensao.tsx
@@ -1,4 +1,4 @@
-import { Link, useForm } from "@inertiajs/react";
+import { Head, Link, useForm } from "@inertiajs/react";
 import { PlusIcon } from "@phosphor-icons/react/dist/ssr/Plus";
 import type { FormEvent } from "react";
 import { toast } from "react-toastify";
@@ -34,67 +34,73 @@ export default function NovoProjetoDeExtensao() {
   };
 
   return (
-    <Main className="my-20">
-      <H1 className="mb-6">Novo Projeto de Extensão</H1>
+    <>
+      <Head>
+        <title>Criar novo projeto de extensão</title>
+      </Head>
 
-      <section className="container-box">
-        <form className="w-full" onSubmit={handleSubmit}>
-          {errors.erro && (
-            <Form.AlertaDeErro className="mb-3">
-              {errors.erro}
-            </Form.AlertaDeErro>
-          )}
+      <Main className="my-20">
+        <H1 className="mb-6">Novo Projeto de Extensão</H1>
 
-          <Form.Input
-            label="Título do Projeto"
-            placeholder="Lorem Ipsum"
-            type="text"
-            name="titulo"
-            onInput={(titulo) => setData({ ...data, titulo })}
-            error={errors.titulo}
-            required
-          />
-
-          <Form.Input
-            label="Data de Início"
-            placeholder="DD/MM/AAAA"
-            type="date"
-            onInput={(value) =>
-              setData({
-                ...data,
-                data_de_inicio: value
-                  ? new Date(value).toJSON().slice(0, 10)
-                  : undefined,
-              })
-            }
-            error={errors.data_de_inicio}
-            observacao="Apenas preencha esse campo se você quiser adicionar uma data customizada."
-          />
-
-          <div className="flex flex-col gap-2">
-            <InputLabelSpan required>Conteúdo</InputLabelSpan>
-            {errors.descricao && (
-              <AlertaDeErro>{errors.descricao}</AlertaDeErro>
+        <section className="container-box">
+          <form className="w-full" onSubmit={handleSubmit}>
+            {errors.erro && (
+              <Form.AlertaDeErro className="mb-3">
+                {errors.erro}
+              </Form.AlertaDeErro>
             )}
-            <TinyMCEEditor
-              initialValue="Descreva o projeto em detalhes."
-              onEditorChange={(html, _editor) =>
-                setData({ ...data, descricao: html })
-              }
-            />
-          </div>
 
-          <div className="flex items-center gap-3 mt-6">
-            <Button.CallToAction type="submit" disabled={processing}>
-              <PlusIcon size={16} weight="bold" />
-              Criar
-            </Button.CallToAction>
-            <Button.Secundario asChild>
-              <Link href="/">Voltar</Link>
-            </Button.Secundario>
-          </div>
-        </form>
-      </section>
-    </Main>
+            <Form.Input
+              label="Título do Projeto"
+              placeholder="Lorem Ipsum"
+              type="text"
+              name="titulo"
+              onInput={(titulo) => setData({ ...data, titulo })}
+              error={errors.titulo}
+              required
+            />
+
+            <Form.Input
+              label="Data de Início"
+              placeholder="DD/MM/AAAA"
+              type="date"
+              onInput={(value) =>
+                setData({
+                  ...data,
+                  data_de_inicio: value
+                    ? new Date(value).toJSON().slice(0, 10)
+                    : undefined,
+                })
+              }
+              error={errors.data_de_inicio}
+              observacao="Apenas preencha esse campo se você quiser adicionar uma data customizada."
+            />
+
+            <div className="flex flex-col gap-2">
+              <InputLabelSpan required>Conteúdo</InputLabelSpan>
+              {errors.descricao && (
+                <AlertaDeErro>{errors.descricao}</AlertaDeErro>
+              )}
+              <TinyMCEEditor
+                initialValue="Descreva o projeto em detalhes."
+                onEditorChange={(html, _editor) =>
+                  setData({ ...data, descricao: html })
+                }
+              />
+            </div>
+
+            <div className="flex items-center gap-3 mt-6">
+              <Button.CallToAction type="submit" disabled={processing}>
+                <PlusIcon size={16} weight="bold" />
+                Criar
+              </Button.CallToAction>
+              <Button.Secundario asChild>
+                <Link href="/">Voltar</Link>
+              </Button.Secundario>
+            </div>
+          </form>
+        </section>
+      </Main>
+    </>
   );
 }
