26275cf5e513e4a84b8bfcabf048a266aba7fcc8
Guedinsi <pedrosg2004@outlook.com>
2025-07-09 23:27:15 -0300

    tabela "vaga" em migrations

--- migrations/20250609042814_cria-tabelas-de-usuarios-e-projetos.down.sql
+++ migrations/20250609042814_cria-tabelas-de-usuarios-e-projetos.down.sql
@@ -2,10 +2,13 @@
 
 DROP INDEX IF EXISTS idx_coordenador_projeto;
 DROP INDEX IF EXISTS coordenadores_unicos_por_projeto;
+DROP INDEX IF EXISTS idx_vaga_coordenador;
+DROP INDEX IF EXISTS idx_vaga_projeto;
 
 DROP TABLE IF EXISTS "coordenador_projeto";
 DROP TABLE IF EXISTS "projeto";
 DROP TABLE IF EXISTS "usuario";
+DROP TABLE IF EXISTS "vaga";
 
 DROP TYPE IF EXISTS tipo_coordenacao_e;
 DROP TYPE IF EXISTS tipo_de_projeto_e;
