a6fcdbdfbec774b06a49b7e6cd39f19b5709146b
KaioFelps <kaiofelipe2005@gmail.com>
2025-07-10 14:18:35 -0300

    chore(www/pages/professores/vagas): remove chamada do console.log

--- www/pages/professores/vagas/nova.tsx
+++ www/pages/professores/vagas/nova.tsx
@@ -24,7 +24,6 @@ export default function CriarNovaVagaDeProjeto() {
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log(JSON.stringify(data));
 
     post("/professores/vagas/criar", {
       onSuccess() {
