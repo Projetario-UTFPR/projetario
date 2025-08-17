1209458b3531ba2629afc9db675a2cb60222d9cc
Kaio Felps <kaiofelipe2005@gmail.com>
2025-08-16 22:15:23 -0300

    chore(test): builda o front-end para realizar os testes se já não tiver sido buildado anteriormente

--- rodar-testes.sh
+++ rodar-testes.sh
@@ -1,2 +1,6 @@
+if ! [ -e public/bundle/manifest.json ]; then
+    npm run build
+fi
+
 # Veja: https://www.reddit.com/r/rust/comments/ny6k3f/cfgtest_doesnt_take_affect_when_running/
 cargo test --features test-utils --release
