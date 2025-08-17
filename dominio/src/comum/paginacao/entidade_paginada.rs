690e1b1311343ede910f983e976c6c0d895d5605
Kaio Felps <kaiofelipe2005@gmail.com>
2025-08-17 20:00:49 -0300

    change(`EntidadePaginada`): requer que `Entidade` implemente a trait `Debug`

--- dominio/src/comum/paginacao/entidade_paginada.rs
+++ dominio/src/comum/paginacao/entidade_paginada.rs
@@ -1,4 +1,7 @@
-pub struct EntidadePaginada<Entidade> {
+use std::fmt::Debug;
+
+#[derive(Debug)]
+pub struct EntidadePaginada<Entidade: Debug> {
     pub dados: Vec<Entidade>,
     pub qtd_total: u64,
 }
