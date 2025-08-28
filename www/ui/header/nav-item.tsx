8fa8692d5aa1a296873a0c27405d115283f050a5
Kaio Felps <kaiofelipe2005@gmail.com>
2025-08-28 20:28:52 -0300

    fix(css): corrige dark mode no header

--- www/ui/header/nav-item.tsx
+++ www/ui/header/nav-item.tsx
@@ -15,7 +15,7 @@ export function NavItem({ label, href }: Props) {
       className={clsx(
         "transition-all duration-150 px-6 font-medium flex items-center justify-center rounded-xl",
         "data-[active=false]:hover:bg-black/5 data-[active=true]:text-gray-600",
-        "dark:text-white data-[active=false]:bg-white/5 data-[active=true]:dark:text-gray-500",
+        "dark:text-white data-[active=false]:dark:hover:bg-white/5 data-[active=true]:dark:text-gray-500",
         "dark:text-shadow-[0_2px_2px_var(--tw-text-shadow-color)] dark:text-shadow-black/15",
         "data-[active=false]:active:brightness-90",
       )}
