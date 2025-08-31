f28bde14c72d7198b2e5be4c334d7d02e64545a5
Kaio Felps <kaiofelipe2005@gmail.com>
2025-08-31 00:02:36 -0300

    chore(TipoDeProjeto): deriva traits extras na estrutura

--- dominio/src/projetos/enums/tipo_de_projeto.rs
+++ dominio/src/projetos/enums/tipo_de_projeto.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize, Debug, Clone, Copy, sqlx::Type)]
+#[derive(Deserialize, Serialize, Debug, Clone, Copy, PartialEq, Eq, Hash, sqlx::Type)]
 #[sqlx(type_name = "tipo_de_projeto_e", rename_all = "snake_case")]
 pub enum TipoDeProjeto {
     Extensao,
