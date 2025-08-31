ed95bf3db19b4f6d5df898e882798154f231926a
Kaio Felps <kaiofelipe2005@gmail.com>
2025-08-31 00:07:23 -0300

    feat: adiciona serviço de buscar vagas de projetos

--- dominio/src/vagas/servicos.rs
+++ dominio/src/vagas/servicos.rs
@@ -1,3 +1,4 @@
 pub mod alterar_vaga;
+pub mod buscar_vagas;
 pub mod cancelar_vaga;
 pub mod criar_vaga;
---
+++ dominio/src/vagas/servicos/buscar_vagas.rs
@@ -0,0 +1,92 @@
+use std::collections::HashSet;
+
+use comum::erros::ResultadoDominio;
+use comum::sqlx::DbDateTime;
+use uuid::Uuid;
+
+use crate::comum::filtragem::LimitadorDeData;
+use crate::comum::paginacao::{Paginacao, PaginacaoMelhorada};
+use crate::projetos::enums::tipo_de_projeto::TipoDeProjeto;
+use crate::vagas::entidades::vaga::Vaga;
+use crate::vagas::filtragem::{EstadoDaVaga, FiltroDeVaga, OrdenacaoDeVaga};
+use crate::vagas::repositorios::vaga::RepositorioDeVagas;
+
+pub struct PossiveisFiltrosParaBuscarVagas {
+    pub titulo: Option<String>,
+    pub tipo: Option<TipoDeProjeto>,
+    pub coordenador: Option<Uuid>,
+    pub data_de_publicacao: Option<(DbDateTime, LimitadorDeData)>,
+    pub estado: Option<EstadoDaVaga>,
+}
+
+pub struct BuscarVagasDeProjetosParams {
+    pub possiveis_filtros: PossiveisFiltrosParaBuscarVagas,
+    pub ordenador: OrdenacaoDeVaga,
+    pub paginacao: Paginacao,
+}
+
+pub struct ServicoBuscarVagasDeProjetos<RV: RepositorioDeVagas> {
+    repositorio_de_vagas: RV,
+}
+
+impl<RV: RepositorioDeVagas> ServicoBuscarVagasDeProjetos<RV> {
+    pub fn novo(repositorio_de_vagas: RV) -> Self {
+        Self {
+            repositorio_de_vagas,
+        }
+    }
+
+    pub async fn executar(
+        &self,
+        BuscarVagasDeProjetosParams {
+            possiveis_filtros,
+            ordenador,
+            paginacao,
+        }: BuscarVagasDeProjetosParams,
+    ) -> ResultadoDominio<PaginacaoMelhorada<Vaga>> {
+        let filtros = Self::transforme_filtros_em_hashset(possiveis_filtros);
+
+        let vagas = self
+            .repositorio_de_vagas
+            .buscar_vagas(filtros, ordenador, paginacao.clone())
+            .await?;
+
+        Ok(PaginacaoMelhorada::nova_a_partir_de_entidade_paginada(
+            vagas,
+            paginacao.qtd_por_pagina,
+            paginacao.pagina,
+        ))
+    }
+
+    fn transforme_filtros_em_hashset(
+        params: PossiveisFiltrosParaBuscarVagas,
+    ) -> HashSet<FiltroDeVaga> {
+        let mut filtros = HashSet::new();
+
+        if let Some(value) = params.coordenador {
+            filtros.insert(FiltroDeVaga::Coordenador(value));
+        }
+
+        if let Some(value) = params.data_de_publicacao {
+            filtros.insert(FiltroDeVaga::DataDePublicacao(value.0, value.1));
+        }
+
+        if let Some(value) = params.estado {
+            filtros.insert(FiltroDeVaga::Estado(value));
+        }
+
+        if let Some(value) = params.titulo {
+            filtros.insert(FiltroDeVaga::Titulo(value));
+        }
+
+        if let Some(value) = params.tipo {
+            filtros.insert(FiltroDeVaga::Tipo(value));
+        }
+
+        filtros
+    }
+}
+
+// TODO: implementar testes unitários do serviço de buscar vagas de projetos
+#[cfg(test)]
+mod test {}
