43e6f287ae79ac2e3e956324ea9010a37398c01e
Kaio Felps <kaiofelipe2005@gmail.com>
2026-02-20 21:22:54 -0300

    feat: adiciona método-fábrica para obter o serviço de buscar vagas

---
+++ src/infra/fabricas/servicos/buscar_vagas.rs
@@ -0,0 +1,11 @@
+use dominio::vagas::servicos::buscar_vagas::ServicoBuscarVagasDeProjetos;
+use sqlx::PgPool;
+
+use crate::infra::repositorios::sqlx::vagas::RepositorioDeVagasSQLX;
+
+pub fn obtenha_servico_buscar_vagas(
+    db_conn: &PgPool,
+) -> ServicoBuscarVagasDeProjetos<RepositorioDeVagasSQLX> {
+    let repositorio_de_vagas = RepositorioDeVagasSQLX::novo(db_conn);
+    ServicoBuscarVagasDeProjetos::novo(repositorio_de_vagas)
+}
--- src/infra/fabricas/servicos/mod.rs
+++ src/infra/fabricas/servicos/mod.rs
@@ -1,4 +1,5 @@
 pub mod autenticacao;
 pub mod buscar_projeto_de_extensao;
+pub mod buscar_vagas;
 pub mod criar_projeto_de_extensao;
 pub mod criar_vaga;
