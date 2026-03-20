9e2b1709e7055b5d30e38c73f09c884d1dfcfcec
Kaio Felps <kaiofelipe2005@gmail.com>
2026-03-20 19:44:00 -0300

    fix(`FiltroDeTipoDeVaga`): atualiza corretamente o estado do filtro de tipo de vaga/projeto

--- www/ui/vagas/index/filtro-de-tipo-de-vaga.tsx
+++ www/ui/vagas/index/filtro-de-tipo-de-vaga.tsx
@@ -1,12 +1,16 @@
-import * as Toggle from "@radix-ui/react-toggle";
-import clsx from "clsx";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import type { TipoDeProjeto } from "@/core/types/enums/tipo-de-projeto";
 import { Item } from "./item";
 
 type Props = {
   onValueChange: (value: TipoDeProjeto | null) => void;
+  /**
+   * The search params key that represents the values of this filter.
+   */
+  paramKey: string;
 };
+
+const TIPOS_DE_PROJETO = ["Extensao", "IniciacaoCientifica"] as const satisfies TipoDeProjeto[];
 
 function resolvaFiltragemExclusivaPorTipoDeVaga(_tiposPermitidos: TipoDeProjeto[]): TipoDeProjeto | null {
   const tiposPermitidos = new Set(_tiposPermitidos);
@@ -18,28 +22,49 @@ function resolvaFiltragemExclusivaPorTipoDeVaga(_tiposPermitidos: TipoDeProjeto[
   return tipoRestante;
 }
 
-export function FiltroDeTipoDeVaga({ onValueChange }: Props) {
-  const [tiposSelecionados, setTiposSelecionados] = useState<TipoDeProjeto[]>([]);
+function obtenhaListaInicialDeTiposSelecionados(paramKey: string): TipoDeProjeto[] {
+  const searchParams = new URLSearchParams(window.location.search);
+  const tipoJaSelecionado = searchParams.get(paramKey);
 
-  useEffect(() => {
-    const tipoExclusivo = resolvaFiltragemExclusivaPorTipoDeVaga(tiposSelecionados);
+  if (!tipoJaSelecionado) return [...TIPOS_DE_PROJETO];
+
+  const tipoJaSelecionadoEhValido = TIPOS_DE_PROJETO.includes(tipoJaSelecionado as TipoDeProjeto);
+
+  if (!tipoJaSelecionadoEhValido) return [...TIPOS_DE_PROJETO];
+
+  return [tipoJaSelecionado as TipoDeProjeto];
+}
+
+export function FiltroDeTipoDeVaga({ onValueChange, paramKey }: Props) {
+  const [tiposSelecionados, setTiposSelecionados] = useState<TipoDeProjeto[]>(
+    obtenhaListaInicialDeTiposSelecionados(paramKey),
+  );
+
+  const calculeNovosTiposSelecionados = (value: TipoDeProjeto, pressed: boolean) => {
+    if (pressed) return [...tiposSelecionados, value];
+    return tiposSelecionados.filter((tipo) => tipo !== value);
+  };
+
+  const atualizarTiposSelecionados = (value: TipoDeProjeto, pressed: boolean) => {
+    const novosTipos = calculeNovosTiposSelecionados(value, pressed);
+    const tipoExclusivo = resolvaFiltragemExclusivaPorTipoDeVaga(novosTipos);
+    setTiposSelecionados(novosTipos);
     onValueChange(tipoExclusivo);
-  }, [tiposSelecionados, onValueChange]);
-
-  const toggle = (value: TipoDeProjeto, pressed: boolean) => {
-    if (pressed) return setTiposSelecionados((tipos) => [...tipos, value]);
-    setTiposSelecionados((tipos) => tipos.filter((tipo) => tipo !== value));
   };
 
   return (
-    <div className="shrink-0 flex items-center justify-center gap-[5px]">
-      <Item value="Extensao" pressed={tiposSelecionados.includes("Extensao")} toggle={toggle}>
+    <div className="shrink-0 flex items-center justify-center gap-1.25">
+      <Item
+        value="Extensao"
+        pressed={tiposSelecionados.includes("Extensao")}
+        toggle={atualizarTiposSelecionados}
+      >
         Extensão
       </Item>
       <Item
         value="IniciacaoCientifica"
         pressed={tiposSelecionados.includes("IniciacaoCientifica")}
-        toggle={toggle}
+        toggle={atualizarTiposSelecionados}
       >
         Iniciação científica
       </Item>
