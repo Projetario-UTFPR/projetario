dc67952c0ccd53f34833ed77b85e7283886c6ede
Kaio Felps <kaiofelipe2005@gmail.com>
2026-03-21 01:16:50 -0300

    linting: corrige erro de elisão de lifetimes do rust fmt

--- src/infra/fabricas/servicos/buscar_projeto_de_extensao.rs
+++ src/infra/fabricas/servicos/buscar_projeto_de_extensao.rs
@@ -4,8 +4,8 @@ use sqlx::PgPool;
 use crate::infra::repositorios::sqlx::projetos::RepositorioDeProjetosSQLX;
 
 pub fn obtenha_servico_buscar_de_projetos_de_extensao(
-    db_conn: &PgPool,
-) -> ServicoBuscarProjetoDeExtensao<RepositorioDeProjetosSQLX> {
+    db_conn: &'_ PgPool,
+) -> ServicoBuscarProjetoDeExtensao<RepositorioDeProjetosSQLX<'_>> {
     let repositorio_projetos = RepositorioDeProjetosSQLX::novo(db_conn);
     ServicoBuscarProjetoDeExtensao::novo(repositorio_projetos)
 }
